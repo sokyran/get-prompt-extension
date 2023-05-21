f331bb80395785fc6868f4243176db5616b62032
“sokyran” <karass0001@gmail.com>
2023-05-21 22:14:12 +0300

    fix: correct param for save_model

--- server/save_model.py
+++ server/save_model.py
@@ -3,8 +3,8 @@ from transformers import BlipProcessor, BlipForConditionalGeneration
 
 models_path = 'models/'
 
-processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base", local_files_only=True)
-model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base", local_files_only=True)
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base", local_files_only=False)
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base", local_files_only=False)
 
 processor.save_pretrained(models_path + 'processor')
 model.save_pretrained(models_path + 'model')
